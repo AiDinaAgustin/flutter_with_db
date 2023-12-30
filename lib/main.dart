bea2bab04e955542f7e0bc9768493f4ceec24db0
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-30 21:58:50 +0700

    langkah 9-12

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import '../pages/mahasiswa_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter With DB',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MahasiswaPage(),
     );
   }
 }
